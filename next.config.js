7a14c09c437548220665e75df8cb67f1ca088bea
jaziel1974 <jaziel1974@gmail.com>
2024-10-10 06:47:54 -0300

    build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'standalone',
+  reactStrictMode: false,
   compiler: {
     styledComponents: true,
   },
--- shared/crypto.js
+++ shared/crypto.js
@@ -1,15 +1,15 @@
-import CryptoJs from 'crypto-js';
+//import CryptoJs from 'crypto-js';
 
 const cryptoProps = {
     enc: 'vOVH6qtSWrUqGbJQPEwVeQ',
-    iv: CryptoJs.lib.WordArray.random(128 / 8)
+    //iv: CryptoJs.lib.WordArray.random(128 / 8)
 }
 
 export function encrypt(text) {
-    return CryptoJs.AES.encrypt(JSON.stringify(text), cryptoProps.enc);
+    return "";//CryptoJs.AES.encrypt(JSON.stringify(text), cryptoProps.enc);
 }
 
 export function decrypt(text) {
-    const bytes =  CryptoJs.AES.decrypt(text, cryptoProps.enc)
-    return JSON.parse(bytes.toString(CryptoJs.enc.Utf8));
+    //const bytes =  CryptoJs.AES.decrypt(text, cryptoProps.enc)
+    return ""; JSON.parse(bytes.toString(CryptoJs.enc.Utf8));
 }
