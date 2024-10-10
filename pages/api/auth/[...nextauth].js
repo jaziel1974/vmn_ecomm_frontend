4e73d6213014c2276c5e0939568abf7859539892
jaziel1974 <jaziel1974@gmail.com>
2024-10-10 10:04:31 -0300

    build error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,34 +1,14 @@
-import { connectDB } from "@/lib/mongodb";
-import User from "@/models/User";
-import credentials from "next-auth/providers/credentials";
-import { decrypt, encrypt } from "../../../shared/crypto";
+import NextAuth, { getServerSession } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
-    credentials({
-      name: "Credentials",
-      id: "credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        await connectDB();
-        const user = await User.findOne({
-          email: credentials?.email,
-        }).select("+password");
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
+  ]
+}
 
-        if (!user) throw new Error("Wrong Email");
-
-        const encrPass = encrypt(password);
-        const passwordMatch = user.data?.password === encrPass;
-
-        if (!passwordMatch) throw new Error("Wrong Password");
-        return user;
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  }
-};
+export default NextAuth(authOptions)
