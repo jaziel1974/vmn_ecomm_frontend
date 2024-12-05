cdfa8da56111bc43715d8a13e04346d8d143deae
jaziel1974 <jaziel1974@gmail.com>
2024-12-05 15:23:03 -0300

    200 shipment cost modification

--- components/ShippingEngine.js
+++ components/ShippingEngine.js
@@ -7,7 +7,7 @@ export const calculateShipping = (freigthType, totalCost, quantity) => {
         case '100':
             return totalCost < 100 ? 10 : 0;
         case '200':
-            return totalCost < 100 ? 20 : totalCost < 200 ? 10 : 0;
+            return totalCost < 80 ? 20 : totalCost < 150 ? 10 : 0;
         default:
             return 0;
     }
